352846dc1a442233330a54b265cfabbddf77f074
DurgaSwaroop <durgaswaroop@gmail.com>
2017-11-29 00:37:56 +0530

    Add dummy shrink method in shrinkers_controller
    This will do the actual shrinking.

--- app/controllers/shrinkers_controller.rb
+++ app/controllers/shrinkers_controller.rb
@@ -1,2 +1,12 @@
 class ShrinkersController < ApplicationController
+  # Just for testing it with postman. Remove it once done.
+  protect_from_forgery except: :shrink
+
+
+  def shrink
+    puts params
+    original_url = params[:original_url]
+    print original_url
+  end
+
 end
