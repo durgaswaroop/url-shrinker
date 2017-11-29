3691d61028c375630d8f292878f3b37ecde3ba29
DurgaSwaroop <durga.perla@oracle.com>
2017-11-29 15:28:13 +0530

    Add route to create a Url object
    With resources :urls, only: [:create], we create only the "create" action and not the usual "new", "edit", "update" ...

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,14 @@
 class UrlsController < ApplicationController
+  def create
+    # Create a new url object from the parameters dictionary
+    puts @url
+    @url = Url.new(url_params)
+  end
+
+  # Url params.
+  private
+  def url_params
+    params.require(:url).permit(:original)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+
+  # post 'shrink', to: 'urls#shrink'
+  resources :urls, only: [:create]
+
   root 'home#index'
 end
