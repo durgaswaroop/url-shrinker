8a48037d22fb104c77e5b4604ed32ba4cea59c2d
DurgaSwaroop <durgaswaroop@gmail.com>
2018-01-25 03:43:23 +0530

    Add POST method to get the details from the form when submitted
    Changed the HTML to have form method as POST.
    Created the method index_post() which will be triggered when we hit submit. Right now we'll just return the URL back to the User. Have to change that.

--- Router.py
+++ Router.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    url = request.form['url']
+    return url
 
 
 def main():
