00d2dc4eba29d11ffc55734e75b1d64ebb7f3d84
DurgaSwaroop <durgaswaroop@gmail.com>
2018-01-26 01:10:55 +0530

    Add a simple test for the home page

--- project/tests/RouterTest.py
+++ project/tests/RouterTest.py
@@ -10,5 +10,13 @@ class RouterTest(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_home_page(self):
+        raw_html = self.app.get('/').data
+        soup = bs(raw_html, "html.parser")
+        # print(soup)
+
+        assert soup.find('title').text == "Swaroop's URL Shrinker"
+        assert soup.find('h1').text == "Swaroop's URL Shrinker"
+
 if __name__ == '__main__':
     unittest.main()
